7978a982a2feb36b7c3e63f2ee4665dbdff7baa5
ZealousNarwhal <mookman909@hotmail.com>
2026-05-01 15:20:32 -0400

    foundations/03 completed

--- foundations/03_numberChecker/numberChecker.js
+++ foundations/03_numberChecker/numberChecker.js
@@ -1,5 +1,7 @@
 function numberChecker(number) {
-  if (number === 6) {
+  if (number >= 10 &&
+    typeof number === 'number'
+  ) {
     return true;
   } else {
     return false;
--- foundations/03_numberChecker/numberChecker.spec.js
+++ foundations/03_numberChecker/numberChecker.spec.js
@@ -1,7 +1,7 @@
 const numberChecker = require('./numberChecker')
 
 describe('numberChecker', () => {
-  test('returns true when number is 1000', () => {
+  test.skip('returns true when number is 1000', () => {
     expect(numberChecker(1000)).toEqual(true);
   });
   test.skip('returns true when number is 10', () => {
@@ -12,5 +12,10 @@ describe('numberChecker', () => {
   });
   test.skip('returns false when number is 6', () => {
     expect(numberChecker(6)).toEqual(false);
+  });
+  test('returns true when number is 10 or greater', () => {
+    expect(numberChecker(9)).toEqual(false);
+    expect(numberChecker(10)).toEqual(true);
+    expect(numberChecker(1000)).toEqual(true);
   });
 });

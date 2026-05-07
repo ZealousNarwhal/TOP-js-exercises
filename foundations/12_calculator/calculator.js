4d90e0b1b29cf09c6ee56ae826cee4c0cc60f261
ZealousNarwhal <mookman909@hotmail.com>
2026-05-07 15:01:02 -0400

    foundations/12 completed

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return (typeof(a) === "number" && typeof(b) === "number") ? a + b : false;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return (typeof(a) === "number" && typeof(b) === "number") ? a - b : false;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.filter((element) => typeof(element) === "number")
+  .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  const initialValue = 0;
+  return array.filter((element) => typeof(element) === "number")
+  .reduce((accumulator, currentValue) => accumulator * currentValue, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return (typeof(a) === "number" && typeof(b) === "number") ? a ** b : false;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  if(typeof(x) ==="number"){
+    return (x < 2) ? 1 : x*factorial(x-1);
+  }
+	else {
+    return false;
+  }
 };
 
 // Do not edit below this line

a48d149b63693484132ca95c3ada6b7862697d82
ZealousNarwhal <mookman909@hotmail.com>
2026-05-08 01:31:49 -0400

    foundations complete

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+const CalculateAge = function(birth, death){
+    return death ? death-birth : new Date().getFullYear()-birth;
+}
 
+const findTheOldest = function(people) {
+    return people.reduce((oldest, current) => {
+        return CalculateAge(oldest.yearOfBirth,oldest.yearOfDeath) > CalculateAge(current.yearOfBirth,current.yearOfDeath) ?
+        oldest:
+        current;
+    });
+
+    // Bonus Solution! :D
+    // let oldestPerson = {};
+    // let oldestAge = 0;
+    // 
+    // people.forEach(person => {
+    //     const yearOfDeath = person.yearOfDeath ? person.yearOfDeath : new Date().getFullYear();
+    //     const age = yearOfDeath-person.yearOfBirth;
+    //     if(age > oldestAge){
+    //         oldestPerson = person;
+    //         oldestAge = age;
+    //     }
+    // });
+    // return oldestPerson;
 };
 
 // Do not edit below this line

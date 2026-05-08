f9dd693e76a8040ac1e2020be69e932ccc4087bd
ZealousNarwhal <mookman909@hotmail.com>
2026-05-08 00:25:34 -0400

    foundations/14 completed

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    const cleanNumber = parseInt(x);
+    console.log(cleanNumber);
+    if(cleanNumber < 0)return "OOPS";
+    else if(cleanNumber === 0) return 0;
 
+    let lastNumber = 1;
+    let secondLastNumber = 1;
+    let result = 1;
+    for(let i = 2; i < cleanNumber; i++) { //skip the first two numbers (they are always 1)
+        result = lastNumber + secondLastNumber;
+        secondLastNumber = lastNumber;
+        lastNumber = result;
+    }
+    return result;
 };
 
 // Do not edit below this line

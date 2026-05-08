f618615ccdc70d5a7dcc9cf5cd63a568def6e8b7
ZealousNarwhal <mookman909@hotmail.com>
2026-05-07 23:49:28 -0400

    foundations/13 completed

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const cleanString = string.toLowerCase().replace(/[^a-zA-Z0-9]/g, ""); //regex still scares me :(
+    console.log(cleanString);
+    for(let i = 0; i < (cleanString.length/2); i++)
+    {
+        if(cleanString.at(i) !== cleanString.at(-i-1))
+        {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
